2a591d10aaa7e799fc66892a939bc4467daf8949
Matthew Villegas <matthew.villegas@knights.ucf.edu>
2017-08-10 23:00:14 -0400

    Update EnemyController.cs

--- EnemyController.cs
+++ EnemyController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour {
+public class Slingshot : MonoBehaviour {
 	private Collider2D hero;
 
 	public void Update()
