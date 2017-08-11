1d3fa0f7d7f22a5eee03ac8e573719f8e2505ae5
Matthew Villegas <matthew.villegas@knights.ucf.edu>
2017-08-10 23:29:37 -0400

    Add files via upload

---
+++ PlayerDeath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Pls make sure this script is attached to the player and you have made a build with at least one build index
+public class PlayerDeath : MonoBehaviour
+{
+    void Update()
+    {
+        if (transform.position.y < -8)
+            Die();
+    }
+
+    void Die()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Debug.Log("Player has Died");
+    }
+}
