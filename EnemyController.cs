a0aab68142dfc00c7d5b51a6219584e30e897d8c
yashBhosale <bhosaley5@gmail.com>
2017-08-10 20:56:00 -0400

    Add files via upload

---
+++ EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+	private Collider2D hero;
+
+	public void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.E))
+		if(hero != null)
+			hero.gameObject.GetComponent<Player>().slingShot(transform.position);
+	}
+	public  void OnTriggerEnter2D(Collider2D other)
+	{
+		hero = other;
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		hero = null;
+	}
+}
